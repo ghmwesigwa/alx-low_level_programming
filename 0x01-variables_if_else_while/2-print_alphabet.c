6137635d7af42afac6413c884b84b1261f907d97
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-01 13:09:55 +0300

    print alphabets 'a' - 'z'

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
-/**
- *main - prints alphabets in lowercase, followed by a new line
- *Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	for (char alph = 'a'; alph <= 'z'; alph++)
-	{
-		putchar(alph);
-	}
-	putchar('\n');
-	return (0);
-}
+#include<stdio.h>                                                                                                                             
+/**                                                                                                                                           
+ *main - prints alphabets in lowercase, followed by a new line                                                                                
+ *Return: Always 0 (Success)                                                                                                                  
+ */                                                                                                                                           
+                                                                                                                                              
+int main(void)                                                                                                                                
+{                                                                                                                                             
+        char alph = 'a';                                                                                                                      
+        for ( ; alph <= 'z'; alph++)                                                                                                          
+        {                                                                                                                                     
+                putchar(alph);                                                                                                                
+        }                                                                                                                                     
+        putchar('\n');                                                                                                                        
+                                                                                                                                              
+        return(0);                                                                                                                            
+} 
