993eb9535e983e9c45dc23c13cf03e48bf62cc1a
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-01 17:57:58 +0300

    print all combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - print all combinations of three different digits
- *Return: Always 0 (Success)
- */
+*main - print all combinations of three different digits
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n = '0';
