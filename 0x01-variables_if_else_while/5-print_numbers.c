c4f67a22084c4a6f4515bcbc66fc1d9078917f97
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-01 16:42:37 +0300

    prints single digit numbers < 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ *
+ * Return: f(success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for ( ; n <= 9 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
