e73ea1aea3c90ef9bd4f0a3a953782b23c8441d8
George KYAMBADDE <ghmwesigwa@gmail.com>
2022-07-01 02:47:00 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *main - prints alphabets in lowercase, followed by a new line
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph = 'a';
+	for ( ; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	
+	return(0);
+}
