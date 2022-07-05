3fc37091d3a1c2a5de823e252d4ce94f6c8dee99
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-05 14:41:37 +0300

    print the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints lowercase alphabet
- * Return: 0
- */
+* print_alphabet - prints lowercase alphabet
+* Return: 0
+*/
 
 void print_alphabet(void)
 {
