3521053aede06cff9b38736583adf33b32eab4cb
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-05 21:04:43 +0300

    Update 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
+ */
+void print_times_table(int n)
+{
+	int row, column, product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
+				if (product > 99)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar((product / 100) + '0');
+					putchar(((product / 10) % 10) + '0');
+					putchar((product % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar(((product / 10) % 10) + '0');
+					putchar((product % 10) + '0');
+				}
+				else
+				{
+					if (column != 0)
+					{
+						putchar(',');
+						putchar(' ');
+						putchar(' ');
+						putchar(' ');
+					}
+					putchar(product + '0');
+				}
+			}
+			putchar('\n');
+		}
+	}
+}
