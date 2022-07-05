283ce4038ecb51aa9b70b8b3e33e4ccad5e05e8a
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-05 16:24:05 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "2-main.c"
+
+/**
+* print_alphabet_x10 - print lowercase alphabet ten times
+* Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++) /* print the alphabet 10 times*/
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++) /* print the alphabet*/
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
