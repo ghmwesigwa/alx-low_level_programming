2f21951078576515fa7ee1252ba38e9ec77145fc
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-05 21:07:51 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,33 +17,33 @@ void print_times_table(int n)
 				product = row * column;
 				if (product > 99)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar((product / 100) + '0');
-					putchar(((product / 10) % 10) + '0');
-					putchar((product % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
 				}
-				else if (z > 9)
+				else if (product > 9)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar(((product / 10) % 10) + '0');
-					putchar((product % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
 				}
 				else
 				{
 					if (column != 0)
 					{
-						putchar(',');
-						putchar(' ');
-						putchar(' ');
-						putchar(' ');
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
 					}
-					putchar(product + '0');
+					_putchar(product + '0');
 				}
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
