cb0322330e0ecaf63b0ec29f9c7a5743d3325f8d
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-05 18:37:03 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -26,31 +26,3 @@ void jack_bauer(void)
 		}
 	}
 }
-=======
-#include "main.h"
-/**
- * jack_bauer - prints every minute of jack bauer 00:00 to 23:59
- *
- * Return: (void)
- *
- */
-
-void jack_bauer(void)
-{
-	int hourDigit = 0;
-	int minuteDigit = 0;
-
-	for (; hourDigit < 24; hourDigit++)
-	{
-		for (minuteDigit = 0; minuteDigit < 60; minuteDigit++)
-		{
-			_putchar('0' + (hourDigit / 10));
-			_putchar('0' + (hourDigit % 10));
-			_putchar(':');
-			_putchar('0' + (minuteDigit / 10));
-			_putchar('0' + (minuteDigit % 10));
-			_putchar('\n');
-		}
-	}
-}
->>>>>>> 488cd1931b2912f5514d0ed2d14a78711719e0fb
