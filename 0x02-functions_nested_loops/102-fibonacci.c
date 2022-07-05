0d27ef10771b7615b680f2dce2a52754f8af445b
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-05 23:00:23 +0300

    Update 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Print alphabet
+ *
+ * Return: <0) for success <1> for failure
+ *
+ */
+
+#include<stdio.h>
+int main(void)
+{
+	int i;
+	long a = 1;
+	long b = 2;
+
+	for (i = 1; i <= 50/2; i++)
+	{
+		printf("%li %li ", a, b);
+		a += b;
+		b += a;
+	}
+
+	printf("\n");
+
+	return (0);
+}
