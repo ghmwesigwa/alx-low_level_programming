6b5748161e04fbe4d70da273a25a63f9c4353c30
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-06 00:35:55 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,14 +9,13 @@ int main(void)
 	int a = 1, b = 1, sum = 0;
 	int sum_of_evens = 0;
 
-	do
-	{
+	do{
 		sum = a + b;
 		a = b;
 		b = sum;
 		if (sum % 2 == 0)
 			sum_of_evens += sum;
-	}while (sum < 4000000);
+	} while (sum < 4000000);
 	
 	printf("%d\n", sum_of_evens);
 	return (0);
