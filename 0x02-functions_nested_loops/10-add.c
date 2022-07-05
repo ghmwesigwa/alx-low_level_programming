ed11d94a5e5b93d945a5e384a95d3d7e6a145913
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-05 19:52:50 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 /**
 * add-adds two numbers
 * @x: integer value
@@ -6,7 +6,7 @@
 * Return: the result
 *
 */
-intadd(int x, int y)
+int add(int x, int y)
 {
 	return(x + y);
 }
