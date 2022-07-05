7477ce8dfbed05b6ace8c028d63ea6fc304e814c
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-05 16:56:20 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,13 +14,11 @@ void print_alphabet(void);
 /**
  * print_alphabet_x10 - print lowercase alphabet ten times
  */
-
 void print_alphabet_x10(void);
 
 /**
  * _islower - checks for lowercase
  */
-
 int _islower(int c);
  
 #endif
