3b6aca7b48842da93ea6fea363a9b39c037acd42
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-06 02:22:24 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	unsigned long int a_head = 0, a_tail = 1, b_head = 0, b_tail = 2;
-	unsigned long int sum, sum_head;
+	long a_head = 0, a_tail = 1, b_head = 0, b_tail = 2;
+	unsigned long sum, sum_head;
 	int i, overflow;
 
 	printf("%lu, %lu, ", a_tail, b_tail);
