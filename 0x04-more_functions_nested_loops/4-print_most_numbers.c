933e66de6f4e7331b17b83c9e9a6ece76dc47656
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-07 23:27:08 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * print_most_numbers - print 0 to 9, skip 2 and 4
 */
@@ -7,7 +6,6 @@
 void print_most_numbers(void)
 {
 	char n;
-
 	for (n = '0'; n <= '9'; n++)
 	{
 		if (n != '2' && n != '4')
