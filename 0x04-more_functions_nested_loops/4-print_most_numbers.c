c7bfafd4f139673169733a02240bc74bd55d7f3f
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-07 22:00:59 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,18 +3,16 @@
 /**
 * print_most_numbers - print 0 to 9, skip 2 and 4
 */
-
 void print_most_numbers(void)
 {
 	char n = '0';
-
 	while (n <= '9')
 	{
-	 if (n != '2' && n != '4'){
-		_putchar(n);
-	
-	 }
-	 n++;
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+		n++;
 	}
 	_putchar('\n');
 }
