36f53f0a4d397670d7cb088cf747477e980e9b01
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-07 22:13:36 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_triangle - print traingle with #'s with given size
- * @size: size of triangle to draw
- */
+* print_triangle - print traingle with #'s with given size
+* @size: size of triangle to draw
+*/
 
 void print_triangle(int size)
 {
@@ -16,12 +16,12 @@ void print_triangle(int size)
 	for (height = 1; height < size; height++)
 	{
 		for (width = (size - 1); width >= 0; width--){
-		    if (width <= height){
-		        _putchar('#');
-		    }
-		    else {
-		        _putchar(' ');
-		    }
+		 if (width <= height){
+		 _putchar('#');
+		 }
+		 else {
+		 _putchar(' ');
+		 }
 		}
 		_putchar('\n');
 
