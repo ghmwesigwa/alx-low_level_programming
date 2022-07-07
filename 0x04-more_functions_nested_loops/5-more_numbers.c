d16030011806d870d1f01d6f90c6239945d3f577
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-07 23:43:35 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,19 +3,21 @@
 * more_numbers - prints sequence from 0 to 14 10 times
 * Return: (void)
 */
+
 void more_numbers(void)
 {
 	int i;
-	char j;
-	
-	for (i = 1; i <= 10; i++)
+	int j = 0;
+
+	while (j < 10)
 	{
-		for (j = 0; j <= 14; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j / 10 > 0)
-				_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			if ((i / 10) > 0)
+				_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
 		}
+		j++;
 		_putchar('\n');
 	}
 }
