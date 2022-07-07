b05c4f140a076068cbb2eea1dcc3b7de77252396
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-07 23:23:01 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-
 /**
  * main - print largest prime factor of 612852475143
  * Return: 0 on success
  */
-
 int main(void)
 {
   long int num = 612852475143;
   long int div = 2;
-
   while (div < num)
   {
-    if (num % div == 0) 
+    if (num % div == 0)
     {
       num /= div;
       div = 2;
-    } 
+    }
     else
       div++;
   }
