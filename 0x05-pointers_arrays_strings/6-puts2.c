61d6e5db1a03eaf30f11751261ff71cf8de0d3d5
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-11 22:17:25 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,13 @@
 */
 void puts2(char *str)
 {
-	int i = 0;
-	int count = 0;
-
-	do {
-		count++;
+	int i, count;
+	
+	for (i = 0; str[i] != '\0'; )
+	{
+		count ++;
 		i++;
-	} while ((str[i] != '\0'));
+	}
 	for (i = 0; i <= count - 1 ; i++)
 	{
 		if (i % 2 == 0)
