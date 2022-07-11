0d026809b3429f8f04acedf84a4968e7b091449c
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-11 22:13:38 +0300

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
-* _strcpy - function that copies the string pointed to by src
-* @dest: char pointer
-* @src: char pointer
-* Return: char.
-*/
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
