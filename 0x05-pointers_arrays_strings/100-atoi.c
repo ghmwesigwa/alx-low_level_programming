61c60ed5fe781bfdc7d0305eb5152a1ae557cef4
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-11 23:49:16 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
 * _atoi - Convert a string to integer.
@@ -8,29 +8,29 @@
 
 int _atoi(char *s)
 {
-	int i = 0;
-	int h = 0;
-	int p = -1;
-	
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			p *= -1;
+int i = 0;
+int h = 0;
+int p = -1;
 
-		if (s[i] > 47 && s[i] < 58)
-		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
-			else
-				h = (s[i] - '0') * -1;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+p *= -1;
 
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-				break;
-		}
-		i++;
-	}
-	if (p < 0)
-		h *= -1;
+if (s[i] > 47 && s[i] < 58)
+{
+if (h < 0)
+h = (h * 10) - (s[i] - '0');
+else
+h = (s[i] - '0') * -1;
 
-	return (h);
+if (s[i + 1] < 48 || s[i + 1] > 57)
+break;
+}
+i++;
+}
+if (p < 0)
+h *= -1;
+
+return (h);
 }
