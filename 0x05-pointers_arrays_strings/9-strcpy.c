e81a1aff1e03dc4e2c657971f1e0c2100b929311
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-11 22:09:52 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,10 @@ char *_strcpy(char *dest, char *src)
 	int i = 0;
 
 	do {
-		dest[i] = src[i];
-		i++;
-	} while (src[i] != '\0');
+		*dest = *src;
+		src++;
+		dest++;
+	} while (*src);
 	/* dest[i] = '\0'; */
 
 	return (dest);
