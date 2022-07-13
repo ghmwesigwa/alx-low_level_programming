5bc2926681859f69c4815969e182f77a0d9b1f19
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-13 21:21:08 +0300

    concat dest and src strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates string to other string
- * @dest: destination string
- * @src: source string
- *Return: destination string concatenated
- */
-char * _strcat (char * dest, char * src)
+* _strcat - concatenates string to other string
+* @dest: destination string
+* @src: source string
+*Return: destination string concatenated
+*/
+char *_strcat(char *dest, char *src)
 {
-  int j = 0, i;
-  
-  for (i = 0; dest[i] != '\0';)
-    i++;
-  while (src[j] != '\0')
-    dest[i] = src[j];
-    j++;
-    i++;
-  dest[i] = '\0';
-  return (dest);
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	do {
+		dest[i] = src[j];
+		i++;
+		j++;
+	} while (src[j] != '\0');
+	dest[i] = '\0';
+
+	return (dest);
 }
