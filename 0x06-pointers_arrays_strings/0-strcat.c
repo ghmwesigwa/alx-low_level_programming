ebd67ce652ec7667c4c1344ad204aa137f9d5cb3
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-13 21:12:17 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,15 +11,11 @@ char * _strcat (char * dest, char * src)
   int j = 0, i;
   
   for (i = 0; dest[i] != '\0';)
-  {
     i++;
-  }
   while (src[j] != '\0')
-  {
     dest[i] = src[j];
     j++;
     i++;
-  }
   dest[i] = '\0';
   return (dest);
 }
