9cbd19035de404c033e8799a53bf1d37185b79d6
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-13 20:23:50 +0300

    concat two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,24 @@
 #include "main.h"
-/**
-* _strcat - concatenates string to other string
-* @dest: destination string
-* @src: source string
-*Return: destination string concatenated
-*/
-char *_strcat(char *dest, char *src)
-{
-	int i, j = 0;
 
-	for (i = 0; dest[i] != '\0';)
+/**
+ * _strcat - concatenates string to other string
+ * @dest: destination string
+ * @src: source string
+ *Return: destination string concatenated
+ */
+char * _strcat(char * dest, char * src) {
+  int i, j = 0;
+
+  for (i = 0; dest[i] != '\0';)
   {
-		i++;
-    }
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+    i++;
+  }
+  while (src[j] != '\0')
+  {
+    dest[i] = src[j];
+    j++;
+    i++;
+  }
+  dest[i] = '\0';
+  return (dest);
 }
