9cf140984301171ee96318f80e3a1b1a371da0aa
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-15 00:54:49 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,1 +1,22 @@
-
+#include "main.h"
+/**
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+void print_number(int n)
+{
+	unsigned int num;
+/*check if number is negative*/
+	num = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		num = n * -1;
+	}
+/* print number by recursion*/
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
