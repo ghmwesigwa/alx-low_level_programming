501d94eaf23321f853b9b50d5e36e138ad787b62
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-15 01:55:10 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,26 @@
-#include "main.h"
+#include"main.h"
 /**
-* cap_string - capitalize first letter of each word
-* @s: string to manipulate
-* Return: string
+*cap_string-capitalizefirstletterofeachword
+*@s:stringtomanipulate
+*Return:string
 */
-char *cap_string(char *s)
+char*cap_string(char*s)
 {
-	int i;
+	inti;
 	
-	/* check first index for upper-case */
-	if (s[0] >= 97 && s[0] <= 122)
-		s[0] -= 32;
-	/* if lowercase and prior char is separator, capitalize */
-	for (i = 0; s[i] != '\0'; i++)
+	/*checkfirstindexforupper-case*/
+	if(s[0]>=97&&s[0]<=122)
+		s[0]-=32;
+	/*iflowercaseandpriorcharisseparator,capitalize*/
+	for(i=0;s[i]!='\0';i++)
 	{
-		if ((s[i] >= 97 && s[i] <= 122)
-		 && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
-			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
-			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
-			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
-			|| s[i - 1] == '\n'))
-			s[i] -= 32;
+		if((s[i]>=97&&s[i]<=122)
+		&&(s[i-1]==','||s[i-1]==';'||s[i-1]=='.'||
+			s[i-1]=='!'||s[i-1]=='?'||s[i-1]=='"'||
+			s[i-1]=='('||s[i-1]==')'||s[i-1]=='{'||
+			s[i-1]=='}'||s[i-1]==''||s[i-1]=='\t'
+			||s[i-1]=='\n'))
+			s[i]-=32;
 	}
-	return (s);
+	return(s);
 }
