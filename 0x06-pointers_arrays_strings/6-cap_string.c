824464e946b571d0dc04d91c00ec924c2cd326d4
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-14 22:16:12 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,9 +11,9 @@ char *cap_string(char *s)
 	/* check first index for upper-case */
 	if (s[0] >= 97 && s[0] <= 122)
 		s[0] -= 32;
+	/* if lowercase and prior char is separator, capitalize */
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		/* if lowercase and prior char is separator, capitalize */
 		if ((s[i] >= 97 && s[i] <= 122)
 		 && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
 			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
