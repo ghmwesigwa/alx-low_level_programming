5c65aa8fbb4cac8410b1d6c7d778b4a49ed9ef62
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-14 22:14:06 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,21 +6,21 @@
 */
 char *cap_string(char *s)
 {
-int i;
-
-/* check first index for upper-case */
-if (s[0] >= 97 && s[0] <= 122)
-s[0] -= 32;
-for (i = 0; s[i] != '\0'; i++)
-{
-/* if lowercase and prior char is separator, capitalize */
-if ((s[i] >= 97 && s[i] <= 122)
- && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
-s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
-s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
-s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
-|| s[i - 1] == '\n'))
-s[i] -= 32;
-}
-return (s);
+	int i;
+	
+	/* check first index for upper-case */
+	if (s[0] >= 97 && s[0] <= 122)
+		s[0] -= 32;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/* if lowercase and prior char is separator, capitalize */
+		if ((s[i] >= 97 && s[i] <= 122)
+		 && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] -= 32;
+	}
+	return (s);
 }
