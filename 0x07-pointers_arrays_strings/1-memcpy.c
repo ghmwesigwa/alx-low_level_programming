29111b4680e226dd62670d79de96d1d1d726f3c2
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-18 23:24:08 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,12 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
- if (n > 0)
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		int i = 0;
-
-		for (; i < n; i++)
-			dest[i] = src[i];
+		dest[i] = src[i];
+		i++;
 	}
-
 	return (dest);
 }
