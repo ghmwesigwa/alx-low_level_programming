d49b7d58a2ea4b2c49aa978724625fb6f089de05
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-18 19:32:20 +0300

    fill memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,25 +1,21 @@
 #include <main.h>
 /**
- * _memset - main function
- * @s: char pointer var
- * @b: char var
- * @n: int var
- * Return: char return
- */
+* _memset - main function
+* @s: char pointer var
+* @b: char var
+* @n: int var
+* Return: char return
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int size = n; /* accept only positive sizes */
-
-	if (size > 0)
+	if (n > 0)
 	{
-	 int i = 0;
-	 while (i < size)
-	 {
-	 s[i] = b;
-	 i++;
-	 }
-		
+		int i;
+
+		for (i = 0; i < n; i++)
+			*(s + i) = b;
 	}
+
 	return (s);
 }
