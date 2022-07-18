c7463ec750a6a5c753370159305cdad210107c12
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-18 20:20:58 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * _memset - main function
- * @s: char pointer var
- * @b: char var
- * @n: int var
- * Return: char return
- */
+* _memset - main function
+* @s: char pointer var
+* @b: char var
+* @n: int var
+* Return: char return
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
 	if (n > 0)
 	{
-	    unsigned int i = 0;
-	    while (i < n)
-	    {
-	        s[i] = b;
-	        i++;
-	    }
+	 unsigned int i = 0;
+	 while (i < n)
+	 {
+	 s[i] = b;
+	 i++;
+	 }
 		
 	}
 	return (s);
