17c3c00a2d4fb2c43e68f0e5c47c0855aa73da71
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-18 22:37:52 +0300

    Update 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+include "main.h"
+
+/**
+ * set_string - main func
+ * @s: char pointer of pointer s
+ * @to:char pointer
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
