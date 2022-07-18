5c16244470f13f5c21d6e558a8653827771c49e1
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-18 19:42:26 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,10 @@
  *
 char *_memset(char *s, char b, unsigned int n)
 {
-	int size = n; /* accept only positive sizes */
-
-	if (size > 0)
+	if (n > 0)
 	{
-	    int i = 0;
-	    while (i < size)
+	    unsigned int i = 0;
+	    while (i < n)
 	    {
 	        s[i] = b;
 	        i++;
