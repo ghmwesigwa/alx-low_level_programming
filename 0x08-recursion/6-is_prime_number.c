b440c1913cbdc703be7b796d8d5711adf8888c7f
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-20 13:16:48 +0300

    Update 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+
+/**
+* is_prime - calls itself until find if a number is prime
+* @n: number to check if prime
+* @divisor: divisor
+* Return: 1 if prime, 0 if not, or recursive function call
+*/
+
+int is_prime(int n, int divisor)
+{
+	if (n == divisor)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime(n, divisor + 1));
+
+}
+
+/**
+* is_prime_number - check if prime
+* @n: number to check
+* Return: 1 if prime, 0 if not
+*/
+
+int is_prime_number(int n)
+{
+	int divisor = 3;
+
+	if (n == 2) return (1);
+	else if (n % 2 == 0 || n < 2)
+	{
+		return (0);
+	}
+
+	return (is_prime(n, divisor));
+}
