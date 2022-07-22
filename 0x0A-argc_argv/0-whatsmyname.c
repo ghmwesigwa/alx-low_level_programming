c4c7ab3ae449862f904577208af0002eb25c8bcb
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 16:25:19 +0300

    print own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+ /**
+ * main - print name of program
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
