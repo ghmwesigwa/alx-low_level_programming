a7c674a818bcaed083134f71ede76a6eeaeabc16
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 14:41:00 +0300

    Update 0-strcat.c

---
+++ 0x09-static_libraries/c-files/0-strcat.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _strcat - concatenates string from other string
+ * @dest: destination string
+ * @src: source string
+ * Return: destination string concatenated
+ */
+char *_strcat(char *dest, char *src);
