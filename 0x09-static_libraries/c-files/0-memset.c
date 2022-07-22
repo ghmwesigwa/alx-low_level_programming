9b99972b33dcaaf7b795f721a2ca628f579549df
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 14:40:00 +0300

    Update 0-memset.c

---
+++ 0x09-static_libraries/c-files/0-memset.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: pointer memory area
+ * @b: byte to fill
+ * @n: selector number
+ * Return: the pointer used
+ */
+
+char *_memset(char *s, char b, unsigned int n);
