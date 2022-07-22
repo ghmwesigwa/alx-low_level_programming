ac020d3b7184b7a1861af40f08aed8bcbab4c17e
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 18:11:35 +0300

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,10 +1,30 @@
 #include "main.h"
-
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: pointer to checked string
- * @accept: byte to check
- * Return: the number of bytes which consist only of bytes from accept
- */
+* _strspn - return length of string that matches values
+* @s: string to search
+* @accept: target match
+* Return: number of bytes matched
+*/
 
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int matches = 0;
+
+	for (i =0; s[i] != '\0'; i++)
+	{
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				matches++;
+				break;
+			}
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+				return (matches);
+		}
+	}
+	return (matches);
+
+}
