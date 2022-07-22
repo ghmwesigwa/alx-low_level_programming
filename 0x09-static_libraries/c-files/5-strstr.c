cdccd58d1a4ee5581442427598af34a3b487c569
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 14:53:22 +0300

    Update 5-strstr.c

---
+++ 0x09-static_libraries/c-files/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strstr - searches for substring.
+ * @haystack: pointer to checked string
+ * @needle: substring
+ *
+ * Return: a pointer to the first byte of the substring
+ */
+
+char *_strstr(char *haystack, char *needle);
