c2716b38ad4b80225854b458b67c99f0c8a48b31
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 18:15:12 +0300

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
 /**
- * _abs - calculate absolute value for input
- * @n : integer value
- * Return: absolute value of n
- *
- */
+* _abs - calculate absolute value for input
+* @n : integer value
+* Return: absolute value of n
+*
+*/
 
-int _abs(int n);
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	return ((-1) * n);
+}
