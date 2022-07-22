8aa5461bd5bdffa51b62bcae6d05c7f097bdb634
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 18:10:41 +0300

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,8 +1,16 @@
 #include "main.h"
 /**
- * _strcmp - checks if 2 strings are of equal value
- * @s1: destination string
- * @s2: source string to be copied
- * Return: difference of first characters that are of diff value
- */
-int _strcmp(char *s1, char *s2);
+* _strcmp - compare two strings
+* @s1: one string
+* @s2: one string
+* Return: int that tells num spaces in between
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	return (0);
+}
