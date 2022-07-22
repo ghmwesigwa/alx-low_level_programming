a83c6e24ec7a8c91f82a358748fe66ae98b665e2
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 17:31:45 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,13 @@
 * @argv: pointer to array of arguments
 * Return: 0 on success
 */
-
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	for (i = 0 ; i < argc ; i++)
+	{
 		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
