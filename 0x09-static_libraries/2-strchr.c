35fd1abf1e4e77772704b75762fcaad580973bcc
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 18:04:20 +0300

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -4,7 +4,18 @@
  * _strchr - checks for char
  * @s: pointer to first occurrence or NULL
  * @c: character to check
+ *
  * Return: the pointer to first occurrence
  */
 
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
+{
+	unsigned int counter;
+
+	for (counter = 0; s[counter] >= '\0' ; counter++)
+	{
+		if (s[counter] == c)
+			return (s + counter);
+	}
+	return (NULL);
+}
