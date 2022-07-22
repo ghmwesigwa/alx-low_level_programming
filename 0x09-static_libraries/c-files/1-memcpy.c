69faa7bd3949af7f0dc308e7573c2a7ada21c4c1
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 14:42:38 +0300

    Update 1-memcpy.c

---
+++ 0x09-static_libraries/c-files/1-memcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer memory area
+ * @src: byte to copy
+ * @n: selector number
+ * Return: the pointer to destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
