fe545bc9f0b4501c4d4dad1d51ef5cf78027d729
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 18:08:20 +0300

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,11 +1,19 @@
-
 #include "main.h"
 
 /**
- * _islower - check if character is lower case
- * @c : integer value
- * Return: 1 if c is lower case 0 otherwise
- *
+ * _islower - checks for lowercase character
+ * @c: integer value
+ * Return: 1 if lowercase, 0 if uppercase
  */
 
-int _islower(int c);
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
