f06c5e431dcaa87ca8e1e1ca99abb38a66bcc89d
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 14:54:29 +0300

    Update 9-strcpy.c

---
+++ 0x09-static_libraries/c-files/9-strcpy.c
@@ -0,0 +1,10 @@
+
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointed to by src
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: char.
+ */
+char *_strcpy(char *dest, char *src);
