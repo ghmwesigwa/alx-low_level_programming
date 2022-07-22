62ba68c9ec432c3d585c9d2a788f62b578e0fae7
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-22 17:41:23 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,11 @@
 * Return: 0 on success
 */
 
-int main(int argc, char *argv[])
+
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
-		printf("%d\n", --argc);
+		printf("%d\n", argc - 1);
+
 	return (0);
 }
