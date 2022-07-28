e36c827179c186a715e212e7956b8a5f5360d635
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-07-28 22:24:49 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,55 @@
 #include "main.h"
 /**
-* _strlen - calculates string length
-*
-* @s: String character
-* Return: returns string length
-*/
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	for (; *s != '\0'; s++)
-		length++;
-
-	return (length);
-}
-
-/**
 * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
 * @s1: string 1
 * @s2: string 2
 * @n: n bytes to concat from string 2
 * Return: pointer to concatenated string
 */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int s1_length, s2_length, index, count;
+	char *ptr;
+	int num, len, i, j;
 
-	s1_length = _strlen(s1);
-	s2_length = _strlen(s2);
+	num = n;
 
-	str = malloc(s1_length + s2_length * sizeof(char));
-
-	if (str == NULL)
-	{
+	if (s1 == NULL) /* account for NULL strings */
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0) /* account for negative n bytes */
 		return (NULL);
-	}
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
 
-	if(s2 == NULL)
-	{
-		return '\0';
-	}
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-	if (n >= s2_length)
-	{
-		n = s2_length;
-	}
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
+		return (NULL);
 
-	for (index = 0; ((*(str + index) = *s1) && (*s1 != '\0')); s1++, index++)
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+
+	return (ptr);
+}
+
+
+/**
+* _strlen - calculate and return string length
+* @string: string
+* Return: string length
+*/
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
 		;
-
-	for(count = 0, index = s1_length; count <= n; index++, s2++, count++)
-		*(str + index) = *s2;
-
-	return (str);
+	return (i);
 }
