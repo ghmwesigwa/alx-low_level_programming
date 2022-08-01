f71a95b02a870dfa867446ecb121281711239b77
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-01 22:37:39 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+
 /**
 * print_dog - init a dog type
 * @d: struct dog pointer
@@ -8,16 +9,25 @@
 */
 void print_dog(struct dog *d)
 {
-
-	if (d == NULL) /* validate if d initiated correctly */
+	if (d == NULL)
+	{
 		return;
+	}
 	if (d->name != NULL)
+	{
 		printf("Name: %s\n", d->name);
+	}
 	else
+	{
 		printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)\n");
+	}
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
 }
