02858c592ed0efb07273d744f5be1a30522ed1c9
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-03 22:09:45 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,47 +1,46 @@
 #include "3-calc.h"
-
 /**
- * op_add - add
- * @a: integer
- * @b: integer
- * Return: result
- */
+* op_add - add
+* @a: integer
+* @b: integer
+* Return: result
+*/
 
 int op_add(int a, int b) { return (a + b); }
 
 /**
- * op_sub - subtract
- * @a: value to subtract from (int)
- * @b: value to subtract (int)
- *
- * Return: difference (int)
- */
+* op_sub - subtract
+* @a: value to subtract from (int)
+* @b: value to subtract (int)
+*
+* Return: difference (int)
+*/
 
 int op_sub(int a, int b) { return (a - b); }
 
 /**
- * op_div - divides
- * @a: dividend (int)
- * @b: divisor (int)
- *
- * Return: quotient (int)
- */
+* op_div - divides
+* @a: dividend (int)
+* @b: divisor (int)
+*
+* Return: quotient (int)
+*/
 int op_div(int a, int b) { return (a / b); }
 
 /**
- * op_mod - modulo opp
- * @a: dividend (int)
- * @b: divisor (int)
- *
- * Return: remainder (int)
- */
+* op_mod - modulo opp
+* @a: dividend (int)
+* @b: divisor (int)
+*
+* Return: remainder (int)
+*/
 int op_mod(int a, int b) { return (a % b); }
 
 /**
- * op_mul - multiply
- * @a: int
- * @b: int
- *
- * Return: product (int)
- */
+* op_mul - multiply
+* @a: int
+* @b: int
+*
+* Return: product (int)
+*/
 int op_mul(int a, int b) { return (a * b); }
