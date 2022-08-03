140e3d1c81b0d3487d889c43e1ebf6d2b6bcebe1
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-03 22:32:49 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,9 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
-	/* iterate through array and call searching function */
 	for (i = 0; i < size; i++)
-		if (cmp(*(array + i))) /* if returned true */
+		if (cmp(array[i]))
 			return (i);
 
 	return (-1);
