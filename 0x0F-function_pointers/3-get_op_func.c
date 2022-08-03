f9e9ae187584ed10cb9ba0494a40573202d6d088
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-03 21:49:29 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,17 +1,16 @@
 #include "3-calc.h"
 
 /**
- * op_add - adds two numbers
- * @a: int
- * @b: int
- *
- * Return: sum (int)
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: result
  */
 
 int op_add(int a, int b) { return (a + b); }
 
 /**
- * op_sub - subtracts two numbers
+ * op_sub - subtract
  * @a: value to subtract from (int)
  * @b: value to subtract (int)
  *
@@ -21,7 +20,7 @@ int op_add(int a, int b) { return (a + b); }
 int op_sub(int a, int b) { return (a - b); }
 
 /**
- * op_div - divides a by b
+ * op_div - divides
  * @a: dividend (int)
  * @b: divisor (int)
  *
@@ -30,7 +29,7 @@ int op_sub(int a, int b) { return (a - b); }
 int op_div(int a, int b) { return (a / b); }
 
 /**
- * op_mod - calculates modulo
+ * op_mod - modulo opp
  * @a: dividend (int)
  * @b: divisor (int)
  *
@@ -39,7 +38,7 @@ int op_div(int a, int b) { return (a / b); }
 int op_mod(int a, int b) { return (a % b); }
 
 /**
- * op_mul - multiplies two numbers
+ * op_mul - multiply
  * @a: int
  * @b: int
  *
