cf9b61d75541cdea294a6356dde75f7b939a60d3
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-04 13:03:53 +0300

    Update 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+* return sum when given unknown amount
+* of arguments
+* @n: number of args
+* Return: sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int sum = 0. i;
+
+	va_start(valist, n);
+
+	/* iterate through list, update sum */
+for (i = 0; i < n; i++)
+	{
+		sum += va_arg(valist, int);
+	}
+	va_end(valist);
+	return (sum);
+}
