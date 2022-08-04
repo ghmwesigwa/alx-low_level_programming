6db853a9112b118cbe23f033a2c61fef9449cec8
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-04 13:05:26 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int sum = 0. i;
+	unsigned int sum = 0, i;
 
 	va_start(valist, n);
-
+	
 	/* iterate through list, update sum */
-for (i = 0; i < n; i++)
+	
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(valist, int);
 	}
