48b15bec25ccd7639b9539fb06610633aaa3790c
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-04 17:00:27 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,24 +1,26 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints all arguments passed
- * @separator: delimiter
- * @n: amount of arguments in list
- */
+* print_numbers - prints all arguments passed
+* @separator: delimiter
+* @n: amount of arguments in list
+*/
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
-
-	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
+	
+	if (n > 0)
 	{
-		printf("%d", va_arg(valist, int));
-		if (i != n && separator != NULL)
-			printf("%s", separator);
+		va_start(valist, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(valist, int));
+			if (i != n && separator != NULL)
+				printf("%s", separator);
+		}
+		va_end(valist);
 	}
 	printf("\n");
-	va_end(valist);
 }
