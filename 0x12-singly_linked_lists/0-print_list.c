f7e4d90793a7534bbb418a3d2c2b66e76fc2842e
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-11 22:55:56 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * print_list - prints all elements of list_t list
- * @h: linked list
- * Return: number of nodes in linked list
- */
+* print_list - prints all elements of list_t list
+* @h: linked list
+* Return: number of nodes in linked list
+*/
 size_t print_list(const list_t *h)
 {
 	size_t total = 0;
