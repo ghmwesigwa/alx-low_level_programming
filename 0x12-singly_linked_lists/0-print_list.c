67c67571254155c6879ba8940e28bcf592e2a69b
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-11 23:37:51 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,23 @@
 #include "lists.h"
+
 /**
 * print_list - prints all elements of list_t list
 * @h: linked list
 * Return: number of nodes in linked list
 */
+
 size_t print_list(const list_t *h)
 {
-	size_t total = 0;
+	int total = 0;
 
-	if (h)
+	while (h)
 	{
-		while (h)
-		{
-			if (h->str == NULL)
-				printf("[0] %p\n", h->str);
-			else
-				printf("[%d] %s\n", h->len, h->str);
-			total++;
-			h = h->next;
-		}
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		total++;
+		h = h->next;
 	}
 	return (total);
 }
