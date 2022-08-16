b76afcde1d3a4f8143e7bca9d2245adf960bb7c3
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-16 14:56:38 +0300

    Update 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - free a linked list of int
+ * @head: pointer of pointer that contain adress of
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
