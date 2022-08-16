b5b2f131f89c1259c783f423af6342882852e167
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-16 18:19:43 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 0;i < (index - 1) && copy != NULL; i++)
+	for (i = 0; i < (index - 1) && copy != NULL; i++)
 	{
 		copy = copy->next;
 	}
