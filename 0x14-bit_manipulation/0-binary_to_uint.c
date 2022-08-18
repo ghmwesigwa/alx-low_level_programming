23451565ab450138f0bae5b5e98c60694e95cd8d
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-18 20:59:14 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,7 +22,7 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i]; i++)
 	{
 		if ((b[i] != '0') && (b[i] != '1'))
-			return (result);
+			return (0);
 
 		if (b[i] == '1')
 			result += (1 * (1 << len));
