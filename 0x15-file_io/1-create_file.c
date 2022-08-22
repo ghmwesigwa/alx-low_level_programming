9b87d32dc5971881fd9e1e5d5bc5982cc680f31c
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-22 20:56:46 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_return;
+	int fd, print_write;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,8 +23,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
-	write_return = write(fd, text_content, strlen(text_content));
-	if (write_return == -1)
+	print_write = write(fd, text_content, strlen(text_content));
+	if (print_write == -1)
 		return (-1);
 	return (1);
 }
