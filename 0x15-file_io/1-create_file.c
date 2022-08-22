627d899fc54409fe451251678cc59bc5faa1b649
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-22 20:46:45 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,35 @@
 #include "main.h"
 /**
- * create_file - creates file with permissions rw------- and writes content in
- * if file already exists, don't change permissions and just truncate it
- * @filename: name to give to new file
- * @text_content: writes this content into file
- * Return: 1 on success, -1 on error
+ * create_file - create a file and add text in
+ * @filename: name of the file
+ * @text_content: content to add in the file
+ * Return: 1 if success, -1 error
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, print_write;
+	int fd;
+	int print_write;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content == NULL)
-	{
-		/* create with permissions if file doesn't exist, else truncate */
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-		if (fd == -1)
-			return (-1);
-		return (1);
-	}
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	print_write = write(fd, text_content, strlen(text_content));
-	if (print_write == -1)
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	print_write = write(fd, text_content, _strlen(text_content));
+	if (print_write == -1 || print_write != _strlen(text_content))
+	{
+		close(fd);
 		return (-1);
-	close(fd)
+	}
+
+	close(fd);
 	return (1);
 }
