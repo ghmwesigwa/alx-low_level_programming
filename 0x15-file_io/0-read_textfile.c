ef4f6e98a84077897346fcb293d2ebb37c05a0b5
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-08-22 20:31:24 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,26 +8,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_read, n_wrote;
+	int fd, print_read, print_write;
 	char *buffer;
 
-	(filename == NULL) ? return (0) : null;
+	if (filename == NULL)
+		return (0);
+	
 	fd = open(filename, O_RDONLY);
-
-	(fd == -1) ? return (0) : null;
-
+	if (fd == -1)
+		return (0);
+	
 	buffer = malloc(sizeof(char) * letters);
-	(buffer == NULL) ? return (0) : null;
-	n_read = read(fd, buffer, letters);
-	if (n_read == -1)
+	if (buffer == NULL)
+		return (0);
+	
+	print_read = read(fd, buffer, letters);
+	if (print_read == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
-	n_wrote = write(STDOUT_FILENO, buffer, n_read);
-	if (n_wrote == -1)
+
+	print_write = write(STDOUT_FILENO, buffer, print_read);
+	if (print_write == -1)
 	{
 		free(buffer);
 		close(fd);
@@ -35,6 +39,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	close(fd);
-	return (n_read);
+	return (print_read);
+
+}
+	close(fd);
+	return (print_read);
 
 }
