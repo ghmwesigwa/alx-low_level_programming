32baa9a4ed0dd116a759341e61d4e7f4659a8c52
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-09-29 23:43:04 +0300

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,4 +1,5 @@
-#include <list.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -12,8 +13,8 @@ int main(int argc, char *argv[])
 {
 	unsigned int i, b;
 	size_t len, add;
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char p[7] = "      ";
+	char *codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char password[7] = "      ";
 
 	if (argc != 2)
 	{
@@ -21,24 +22,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	len = strlen(argv[1]);
-	p[0] = l[(len ^ 59) & 63];
+	password[0] = codex[(len ^ 59) & 63];
 	for (i = 0, add = 0; i < len; i++)
 		add += argv[1][i];
-	p[1] = l[(add ^ 79) & 63];
+	password[1] = codex[(add ^ 79) & 63];
 	for (i = 0, b = 1; i < len; i++)
 		b *= argv[1][i];
-	p[2] = l[(b ^ 85) & 63];
+	password[2] = codex[(b ^ 85) & 63];
 	for (b = argv[1][0], i = 0; i < len; i++)
 		if ((char)b <= argv[1][i])
 			b = argv[1][i];
 	srand(b ^ 14);
-	p[3] = l[rand() & 63];
+	password[3] = codex[rand() & 63];
 	for (b = 0, i = 0; i < len; i++)
 		b += argv[1][i] * argv[1][i];
-	p[4] = l[(b ^ 239) & 63];
+	password[4] = codex[(b ^ 239) & 63];
 	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
 		b = rand();
-	p[5] = l[(b ^ 229) & 63];
-	printf("%s\n", p);
+	password[5] = codex[(b ^ 229) & 63];
+	printf("%s\n", password);
 	return (0);
 }
