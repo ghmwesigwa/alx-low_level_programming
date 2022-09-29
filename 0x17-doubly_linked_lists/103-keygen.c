6806570de2ca9f42df2ca7c64f103b02e8894ae9
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-09-29 23:47:06 +0300

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -6,7 +6,6 @@
  * main - generate a key depending on a username for crackme5
  * @argc: number of arguments passed
  * @argv: arguments passed to main
- *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
