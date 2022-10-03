ff4a3943380db2e8d8d674a17e6de0f87c450d4f
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-10-03 22:02:41 +0300

    Update 2-strchr.c

--- 0x18-dynamic_libraries/cfiles/2-strchr.c
+++ 0x18-dynamic_libraries/cfiles/2-strchr.c
@@ -1,22 +1,21 @@
+#include <stdio.h>
 #include "main.h"
-#define NULL 0
 
 /**
- * _strchr - locate 1st occurrence of char in string and returns pointer there
- * @s: string to search
- * @c: target characer
- * Return: pointer to that character in string
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
-
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0' && s[i] != c) /* find match */
-		i++;
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
 
-	if (s[i] == c) /* if match, assign to address */
-		return (&s[i]);
-	else
-		return (NULL);
+	return (NULL);
 }
