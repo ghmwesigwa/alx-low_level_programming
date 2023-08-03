e79cee1d41a4dcf8dd94ad7d43d30e8b29c14971
George KYAMBADDE <ghmwesigwa@gmail.com>
2023-08-03 13:04:43 -0700

    update betty style

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,27 +8,27 @@ extern int _putchar(char c);
  */
 void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int started = 0;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int started = 0;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-    while (mask > 0)
-    {
-        if ((n & mask) != 0)
-        {
-            _putchar('1');
-            started = 1;
-        }
-        else if (started == 1)
-        {
-            _putchar('0');
-        }
-        mask >>= 1;
-    }
+	while (mask > 0)
+	{
+		if ((n & mask) != 0)
+		{
+			_putchar('1');
+			started = 1;
+		}
+		else if (started == 1)
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	}
 }
 
