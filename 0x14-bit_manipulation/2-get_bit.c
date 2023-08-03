ba0e6d1212c2efdb48854f87f610155025c3ab61
George KYAMBADDE <ghmwesigwa@gmail.com>
2023-08-03 14:56:23 -0700

    add 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,15 @@
-#include "main.h"
-
 /**
- * get_bit - Returns the value of a bit at a given index.
- * @n: The number to extract the bit from.
- * @index: The index of the bit to get, starting from 0 (LSB).
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The number to check the bit in.
+ * @index: The index of the bit to get, starting from 0.
  *
- * Return: The value of the bit at the given index, or -1 if an error occurs.
+ * Return: The value of the bit at index, or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int mask = 1UL << index;
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
-    if (index >= (sizeof(unsigned long int) * 8))
-        return (-1); // Error: index out of range
-
-    if ((n & mask) != 0)
-        return (1); // The bit at the index is set (1)
-    else
-        return (0); // The bit at the index is not set (0)
+	return ((n >> index) & 1);
 }
 
