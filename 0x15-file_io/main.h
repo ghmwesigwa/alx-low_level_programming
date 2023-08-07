036a5f4744a1d6c4db62dfd06f7d166b688bfde7
George KYAMBADDE <ghmwesigwa@gmail.com>
2023-08-07 13:04:04 -0700

    #3 adding 3-cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,16 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 /* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-void print_error(const char *message, const char *file);
+int main(int argc, char *argv[]);
+int usage_error(void);
 int open_file(const char *filename, int flags);
+void read_error(int fd, const char *filename);
+void write_error(int fd, const char *filename);
+void close_error(int fd);
+void print_error(const char *message, const char *file);
 void copy_file(const char *file_from, const char *file_to);
 
 #endif /* MAIN_H */
-
-
